f92afe731cd80418fb7aaa0d13499455b3ae4d86
Mayanwolfe <cebourdon625@gmail.com>
2022-04-24 20:21:55 -0400

    added snake behavior rules

--- snake_game.js
+++ snake_game.js
@@ -37,3 +37,26 @@ const RIGHT_DIR = 39
 const DOWN_DIR = 40
 
 let snakeCurrentDirection = RIGHT_DIR
+
+//Make sure that the user input is valid and change snake direction variable
+const changeDirection = newDirectionCode => {
+  if(newDirectionCode == snakeCurrentDirection) return;
+
+  if (newDirectionCode == LEFT_DIR && snakeCurrentDirection !== RIGHT_DIR) {
+    snakeCurrentDirection = newDirectionCode
+  } else if(newDirectionCode == UP_DIR && snakeCurrentDirection !== DOWN_DIR) {
+    snakeCurrentDirection = newDirectionCode
+  }else if (newDirectionCode == RIGHT_DIR && snakeCurrentDirection !== LEFT_DIR) {
+    snakeCurrentDirection = newDirectionCode
+  } else if (newDirectionCode == DOWN_DIR && snakeCurrentDirection !== UP_DIR) {
+    snakeCurrentDirection = newDirectionCode
+  }
+}
+
+//set starting point for snake on load
+let currentHeadPosition = TOTAL_PIXEL_COUNT/2
+
+//Set initial length
+let snakeLength = 200
+
+//Start moving snake
